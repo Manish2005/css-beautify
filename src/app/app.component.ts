5eccafac7f5c3e61dfe9c02e0e4a22ad6deb704f
Manish Gharat <gharatmanish@gmail.com>
2020-03-19 22:44:27 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rgb-hex-convertor';
+  form: FormGroup;
+  hexCode: string;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      red: [null, [Validators.required, Validators.min(0), Validators.max(255), this.validateValue]],
+      green: [null, [Validators.required, Validators.min(0), Validators.max(255), this.validateValue]],
+      blue: [null, [Validators.required, Validators.min(0), Validators.max(255), this.validateValue]]
+    });
+  }
+
+  convertToHex() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    let hex = '';
+    Object.values(this.form.value).forEach((num) => {
+      hex += this.intToHex(num);
+    });
+
+    this.hexCode = '#' + hex;
+  }
+
+  validateValue(control: FormControl) {
+    if (control.value && control.value < 0 || control.value > 255) {
+      return { outOfBounds: true };
+    }
+  }
+
+  private intToHex(num) {
+    let hex = Number(num).toString(16);
+    if (hex.length < 2) {
+      hex = '0' + hex;
+    }
+    return hex;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
